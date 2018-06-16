bf102f8ff61d7269800a357224602225a916ae3d
kyu <gkaansrb@coupang.com>
2018-06-16 18:05:53 +0900

    1차 변경

--- ShorteningController.java
+++ ShorteningController.java
@@ -1,5 +1,9 @@
 package com.url.shortening.web;
 
+import com.url.shortening.model.ConvertType;
+import com.url.shortening.service.ShorteningConverterStrategyService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,8 +11,23 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ShorteningController {
 
+	@Autowired
+	private ShorteningConverterStrategyService shorteningConverterStrategyService;
+
 	@RequestMapping(value = "/main", method = RequestMethod.GET)
 	public String main() {
 		return "main";
+	}
+
+	@RequestMapping(value = "/decrypt", method = RequestMethod.POST)
+	public String decrypt(@RequestBody String url) {
+		shorteningConverterStrategyService.convert(ConvertType.DECRYPT, url);
+		return "decrypt";
+	}
+
+	@RequestMapping(value = "/encrypt", method = RequestMethod.POST)
+	public String encrypt(@RequestBody String url) {
+		shorteningConverterStrategyService.convert(ConvertType.ENCRYPT, url);
+		return "encrypt";
 	}
 }
