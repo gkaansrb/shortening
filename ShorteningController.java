5ca334f4579f8ff9c6be79be50e97874744bf573
kyu <gkaansrb@coupang.com>
2018-06-18 23:28:15 +0900

    testCode

--- ShorteningController.java
+++ ShorteningController.java
@@ -5,9 +5,13 @@ import com.url.shortening.model.ConvertType;
 import com.url.shortening.model.UrlMappingDto;
 import com.url.shortening.service.ShorteningConverterStrategyService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-@RestController
+@Controller
 public class ShorteningController {
 
 	@Autowired
