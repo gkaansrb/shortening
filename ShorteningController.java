c0e36a3dcdc331abe5f8b67e37320a3c74bafe2f
kyu <gkaansrb@coupang.com>
2018-06-19 01:45:59 +0900

    testCode

--- ShorteningController.java
+++ ShorteningController.java
@@ -1,21 +1,17 @@
 package com.url.shortening.web;
 
 import com.url.shortening.exception.ShorteningConvertException;
-import com.url.shortening.model.ConvertType;
 import com.url.shortening.model.UrlMappingDto;
-import com.url.shortening.service.ShorteningConverterStrategyService;
+import com.url.shortening.service.ShorteningConverter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class ShorteningController {
 
 	@Autowired
-	private ShorteningConverterStrategyService shorteningConverterStrategyService;
+	private ShorteningConverter shorteningConverter;
 
 	@RequestMapping(value = "/main", method = RequestMethod.GET)
 	public String main() {
@@ -23,14 +19,14 @@ public class ShorteningController {
 	}
 
 	@RequestMapping(value = "/decrypt", method = RequestMethod.GET)
-	public String decrypt(@RequestBody String url) throws ShorteningConvertException {
-		UrlMappingDto convert = shorteningConverterStrategyService.convert(ConvertType.DECRYPT, url);
-		return "redirect:" + convert.getOriginUrl();
+	public String decrypt(@RequestParam(value = "shortUrl") String shortUrl) throws ShorteningConvertException {
+		UrlMappingDto convert = shorteningConverter.findByShortUrl(shortUrl);
+		return convert.getOriginUrl();
 	}
 
 	@RequestMapping(value = "/encrypt", method = RequestMethod.POST)
 	@ResponseBody
-	public UrlMappingDto encrypt(@RequestBody String originUrl) throws ShorteningConvertException {
-		return shorteningConverterStrategyService.convert(ConvertType.ENCRYPT, originUrl);
+	public UrlMappingDto encrypt(@RequestParam(required = true) String originUrl) throws ShorteningConvertException {
+		return shorteningConverter.convert(originUrl);
 	}
 }
